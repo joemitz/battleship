475b575e727c8a486736c6cd7b67b78d80f87a14
Joe Mitzman <joemitz@gmail.com>
2022-01-07 18:25:58 -0800

    Initial commit. Express/React/Webpack/Babel installed and configured

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const root = '/Users/joemitz/Documents/Code/battleship';
+
+app.use(express.static(`${root}/client/`));
+
+app.listen(port, () => {
+  console.log(`Listening at 127.0.0.1:${port}`);
+})
